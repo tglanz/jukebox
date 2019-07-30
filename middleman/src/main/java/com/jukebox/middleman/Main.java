c9ef358afbd80e4ae4b427f0684b145c5a2cd062
Tal Glanzman <talglanzman@gmail.com>
2019-07-30 13:11:39 -0700

    part1(middleman) - shit this was hard

--- middleman/src/main/java/com/jukebox/middleman/Main.java
+++ middleman/src/main/java/com/jukebox/middleman/Main.java
@@ -2,15 +2,8 @@ package com.jukebox.middleman;
 
 // spring
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-
-// kurento
-import org.kurento.client.KurentoClient;
 
 public class Main {
-
-    
-    
     public static void main(String[] args){
         Class<?>[] entries = { Websocket.class };
         SpringApplication.run(entries, args);
--- middleman/src/main/java/com/jukebox/middleman/Websocket.java
+++ middleman/src/main/java/com/jukebox/middleman/Websocket.java
@@ -9,8 +9,6 @@ import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 import org.kurento.client.KurentoClient;
 
