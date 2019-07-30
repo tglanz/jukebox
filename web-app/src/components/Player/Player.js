7d9858687bb8d21f11e538915f780c80b8a626db
Tal Glanzman <talglanzman@gmail.com>
2019-07-30 13:18:41 -0700

    part2(web-app) - shit was hard

--- web-app/src/components/Player/Player.js
+++ web-app/src/components/Player/Player.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
 import kurentoUtils from 'kurento-utils';
+import './Player.css';
 
 const onIceCandidate = ws => candidate => {
-    console.group('onIceCandidate');
+    console.groupCollapsed('onIceCandidate');
 
     const message = {
         id: 'onIceCandidate',
@@ -11,18 +11,17 @@ const onIceCandidate = ws => candidate => {
     };
 
     console.log('message', message);
-    ws.sendMessage(JSON.stringify(message));
-    
+    ws.send(JSON.stringify(message));
+
     console.groupEnd();
 };
 
-
 const onOffer = ws => (error, offerSdp) => {
 	if (error){
-        return console.error('Error generating the offer');
+        return console.error('Error generating the offer', error);
     }
 
-    console.group('onOffer');
+    console.groupCollapsed('onOffer');
 
 	var message = {
 		id : 'start',
@@ -30,89 +29,100 @@ const onOffer = ws => (error, offerSdp) => {
 		videourl : "http://files.openvidu.io/video/format/sintel.webm"
     }
     
-    console.log('message', message);
+    console.log('message', JSON.stringify(message));
+    ws.send(JSON.stringify(message));
 
-    ws.sendMessage(JSON.stringify(message));
-    
     console.groupEnd();
-}
+};
 
 
 const createWs = ({ open, close, error, message }) => {
     // TODO: host
-    const ws = new WebSocket('ws://localhost:8080/audio/websocket');
+    const ws = new WebSocket('wss://localhost:8443/audio/websocket');
     ws.addEventListener('open', open);
     ws.addEventListener('close', close);
     ws.addEventListener('error', error);
     ws.addEventListener('message', message);
     return ws;
-
-}
+};
 
 export default class Player extends React.Component {
 
     constructor(props) {
         super(props);
-        this.videoRef = React.createRef();
+        this.mediaRef = React.createRef();
     }
 
     componentWillMount(){
         this.ws = createWs({
-            open: this.onWsOpen,
-            close: this.onWsClose,
-            error: this.onWsError,
-            message: this.onWsMessage,
+            open: this.onWsOpen.bind(this),
+            close: this.onWsClose.bind(this),
+            error: this.onWsError.bind(this),
+            message: this.onWsMessage.bind(this),
         });
+
+        window.addEventListener('beforeunload', () => this.ws.close());
     }
 
-    componentWillUnmount(){
-        this.ws.close();
-    }
-
-    onWsError(x){
-        console.group("Player::onWsError");
-        console.log('asd');
+    onWsError(error){
+        console.groupCollapsed("Player::onWsError");
+        console.error(error);
         console.groupEnd();
     }
 
     onWsOpen(){
-        console.group("Player::onWsOpen");
+        console.groupCollapsed("Player::onWsOpen");
         console.groupEnd();
     }
 
     onWsClose(){
-        console.group("Player::onWsClose");
+        console.groupCollapsed("Player::onWsClose");
         console.groupEnd();
     }
 
-    onWsMessage(){
-        console.group("Player::onWsMessage");
-        console.groupEnd();
+    onWsMessage(message){
+
+        const data = JSON.parse(message.data);
+        
+        switch (data.id){
+            case 'startResponse':
+                console.log('SDP answer received from server. Processing ...');
+                this.webRtcPeer.processAnswer(data.sdpAnswer, function(error) {
+                    if (error) {
+                        return console.error('Error process message: ' + error);
+                    }
+                });
+                break;
+            case 'iceCandidate':
+                this.webRtcPeer.addIceCandidate(data.candidate, function(error) {
+                    if (error) {
+                        return console.error('Error adding candidate: ' + error);
+                    }
+                });
+                break;
+        }
     }
 
     onStart(){
-        console.group("Player::onStart");
+        console.groupCollapsed("Player::onStart");
         
         const userMediaConstraints = {
             audio : true,
             video : true,
         };
 
-        console.log('userMediaConstraints', userMediaConstraints);
-
         const options = {
-            remoteVideo : this.videoRef.current,
+            remoteVideo : this.mediaRef.current,
             mediaConstraints : userMediaConstraints,
-            onicecandidate : onIceCandidate(this.ws)
+            onicecandidate: onIceCandidate(this.ws),
         };
-
-        console.log('options', options);
 
         this.webRtcPeer = new kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options, error => {
             if (error) {
                 return console.error(error);
             }
-            this.webRtcPeer.generateOffer(onOffer);
+            
+            this.webRtcPeer.generateOffer(onOffer(this.ws));
         });
 
         console.groupEnd();
@@ -122,7 +132,7 @@ export default class Player extends React.Component {
         return (
             <div className="Player">
                 <p>Player</p>
-                <video ref={this.videoRef} />
+                <video id="video" autoPlay width="200px" height="200px" ref={this.mediaRef}></video>
                 <button
                     onClick={this.onStart.bind(this)}>Start</button>
             </div>
--- web-app/src/index.js
+++ web-app/src/index.js
@@ -1,7 +1,10 @@
+import 'webrtc-adapter';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './index.css';
 import App from './components/App/App';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
